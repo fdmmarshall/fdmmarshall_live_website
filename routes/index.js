255b0c6512bb48c5db74352f8ced029cd1250d25
Flor <flordmarshall@gmail.com>
2020-12-14 13:33:32 -0500

    adding route

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/.well-known/pki-validation/AA046C55E9B2C374CC5720B84B1B0863.txt', function (req, res) {
+  res.send('.well-known/pki-vaidation/AA046C55E9B2C374CC5720B84B1B0863.txt')
+});
 module.exports = router;

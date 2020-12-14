f25adad351c0246e584643ed6df7b35dcc5dff3f
Flor <flordmarshall@gmail.com>
2020-12-14 13:47:10 -0500

    test 5

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,6 @@ router.get('/', function (req, res, next) {
 });
 
 router.get('/.well-known/pki-validation/AA046C55E9B2C374CC5720B84B1B0863.txt', function (req, res) {
-  res.render('.well-known/pki-validation/AA046C55E9B2C374CC5720B84B1B0863.txt')
+  res.send('55A1969C1F465FBCAC5BAACBEC768B063186548033FBBF7134B8C37F9B04DB85 comodoca.com 5fd6916273b9d')
 });
 module.exports = router;

1064acf3290325593405a3ab748be93e5b43462f
Flor <flordmarshall@gmail.com>
2020-12-14 14:47:34 -0500

    removing previous commits

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,4 @@ router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/.well-known/pki-validation/AA046C55E9B2C374CC5720B84B1B0863.txt', function (req, res) {
-  res.send('55A1969C1F465FBCAC5BAACBEC768B063186548033FBBF7134B8C37F9B04DB85 comodoca.com 5fd6916273b9d')
-});
 module.exports = router;
